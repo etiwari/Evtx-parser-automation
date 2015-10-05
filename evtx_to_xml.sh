db4d0bad31d4d1d8e0d05c980e47b3804a81463e
Eashaan Tiwari <eashaan.tiwari.1110@gmail.com>
2015-10-05 15:56:07 -0400

    Update evtx_to_xml.sh
    Finished script.

--- evtx_to_xml.sh
+++ evtx_to_xml.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 read -r -p 'Enter Log Directory (dont put / at the end of the directory name): ' directory
 read -r -p 'Enter XML dump location (dont put / at the end of the directory name): ' dumploc
-ls "$directory" > ./data1.dat
-sed -n 's/\.evtx//gpw data2.dat' data1.dat
-sed -n 's/\ /\\ /gpw data.dat' data2.dat
-#rm data1.dat
-#rm data2.dat
+ls "$directory" > ./data.dat
+sed -i 's/\.evtx//g' data.dat
+sed -i 's/\ /\\ /g' data.dat
 while read line
 do
-	./evtxdump.pl "$direcory"/"$line".evtx > "$dumploc"/"$line".xml 
+	./evtxdump.pl "$directory"/"$line".evtx > "$dumploc"/"$line".xml
+	echo "$line done"
 done < data.dat
